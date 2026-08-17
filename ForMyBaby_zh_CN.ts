e6bd401425da04847fba17b6db58aba9103a37aa
peilei <peilei@senseauto.com>
2026-08-17 15:20:36 +0800

    Opt ForMyBaby_zh_CN.ts

--- ForMyBaby_zh_CN.ts
+++ ForMyBaby_zh_CN.ts
@@ -1,3 +1,170 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ForMyBaby</name>
+    <message>
+        <location filename="mainwindow.ui" line="7"/>
+        <location filename="mainwindow.ui" line="151"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="329"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="351"/>
+        <source>For My Baby</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="19"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="346"/>
+        <source>记录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="334"/>
+        <source>照片</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="335"/>
+        <source>请选择一张照片</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="48"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="336"/>
+        <source>选择照片…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="337"/>
+        <source>成长记录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="338"/>
+        <source>记录今天发生的故事……</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="339"/>
+        <source>写下想对宝贝说的话……</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="340"/>
+        <source>时间</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="341"/>
+        <source>yyyy-MM-dd HH:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="342"/>
+        <source>地点</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="93"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="343"/>
+        <source>输入拍摄或记录地点</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="344"/>
+        <source>清空</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="345"/>
+        <source>保存记录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="113"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="350"/>
+        <source>回忆</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="123"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="347"/>
+        <source>记录列表</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="348"/>
+        <source>照片预览</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="349"/>
+        <source>从左侧选择一条记录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="148"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="353"/>
+        <source>关于</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="352"/>
+        <source>珍藏照片、故事与成长中的每一个瞬间</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="354"/>
+        <source>文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="166"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="330"/>
+        <source>打开照片…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="166"/>
+        <location filename="build/ForMyBaby_autogen/include/ui_mainwindow.h" line="332"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Images (*.jpg *jpeg *.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <source>Not selelct any file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
